bebacc22b1df9c9fbf16bca66423f7157fde8050
Rajat Sawarkar <rajat.sawarkar@3pillarglobal.com>
2021-08-08 23:52:49 +0530

    added supertest and some test cases

--- src/__test__/app.test.js
+++ src/__test__/app.test.js
@@ -1,10 +1,45 @@
 // process.env.NODE_ENV = 'test';
 const app = require('../app');
 const request = require('supertest');
-
-describe('dummy', () => {
-	test('just checking env', () => {
-		// expect(reconverted).not.toMatch('sdfasdf');
-		console.log('current env', process.env.NODE_ENV);
+describe('testing /user route apis', function () {
+	test('testing add user api', function (done) {
+		request(app)
+			.post('/users/register')
+			.send({
+				"firstName": "test1",
+				"lastName": "taj",
+				"email": "test1.test1@gmail.com",
+				"password": "abcdef"
+			})
+			.expect(200 || 409)
+			.end(function (err, res) {
+				if (err) return done(err);
+				return done();
+			});
+	});
+	test('testing login user api', function (done) {
+		request(app)
+			.post('/users/login')
+			.send({
+				"email": "test1.test1@gmail.com",
+				"password": "abcdef"
+			})
+			.expect(200)
+			.end(function (err, res) {
+				if (err) return done(err);
+				return done();
+			});
+	});
+	test('testing forgotPassword password user api', function (done) {
+		request(app)
+			.post('/users/forgotPassword')
+			.send({
+				"email": "test1.test1@gmail.com"
+			})
+			.expect(200)
+			.end(function (err, res) {
+				if (err) return done(err);
+				return done();
+			});
 	});
 });
